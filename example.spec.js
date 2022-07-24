d09e9edeb970a57be9b26650ef9390a60afecd8d
Chloe Golovleva <chloinwrite@gmail.com>
2022-07-24 15:08:22 -0300

    updating example.js

--- example.spec.js
+++ example.spec.js
@@ -3,10 +3,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
-
 chai.use(chaiHttp);
 const url = 'http://www.mocky.io/v2/5e9602272f0000740002577c';
-
 describe('Should retrieve information: ', () => {
     it('GET mocky data for name', (done) => {
         chai.request(url)
